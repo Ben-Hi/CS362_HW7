e19c66bf3393248aae3be76a048d280d283f621f
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:31:28 -0800

    Implemented print_fizz method to print 'Fizz' if and only if the output is a multiple of 3

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,7 +1,12 @@
 def fizzBuzz():
     output = []
     for i in range(1, 101):
-        output.append(i)
-        print(i)
+        if i % 3 == 0 and i % 5 != 0:
+            output.append('Fizz')
+            print('Fizz')
+            
+        else:
+            output.append(i)
+            print(i)
 
     return output
