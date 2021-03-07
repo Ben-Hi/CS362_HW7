49e514067a59b7af6b5ce2392dab327e9464702e
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:57:18 -0800

    Wrote 3rd leapYear test: 400 is evenly divisible by 4, 100, and 400, so the code should say that 400 is a leap year

--- test_leapYear.py
+++ test_leapYear.py
@@ -8,5 +8,8 @@ class Test_LeapYear(unittest.TestCase):
     def test_div_by_100_not_400(self):
         self.assertEqual(leapYear.leapYear(500), '500 is not a leap year')
 
+    def test_div_by_100_and_400(self):
+        self.assertEqual(leapYear.leapYear(400), '400 is a leap year')
+
 if __name__ == '__main__':
     unittest.main()
