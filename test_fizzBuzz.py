292d74ecd043bdde78fc7a04dc0aac46e76fd4e3
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:19:36 -0800

    1st fizzBuzz test: output 1-100, ignore fizzbuzz cases

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -1,2 +1,13 @@
 import unittest
 import fizzBuzz
+
+class Test_FizzBuzz(unittest.TestCase):
+    #Confirm that fizzBuzz prints integers 1-100 aside from the special fizz/buzz cases
+    def test_print_to_100(self):
+        output = fizzBuzz.fizzBuzz()
+        for i in range (1, 101):
+            if i % 3 != 0 and i % 5 != 0:
+                self.assertEqual(output[i - 1], i)
+
+if __name__ == '__main__':
+    unittest.main()
