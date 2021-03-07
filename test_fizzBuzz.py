2f0508dd3648395d921b20322ad46f36a3e8d3ec
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:29:04 -0800

    Wrote 2nd fizzBuzz test: all multiples of 3 are 'Fizz' instead of integers

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -5,9 +5,16 @@ class Test_FizzBuzz(unittest.TestCase):
     #Confirm that fizzBuzz prints integers 1-100 aside from the special fizz/buzz cases
     def test_print_to_100(self):
         output = fizzBuzz.fizzBuzz()
-        for i in range (1, 101):
+        for i in range(1, 101):
             if i % 3 != 0 and i % 5 != 0:
                 self.assertEqual(output[i - 1], i)
+
+    #Confirm that fizzBuzz prints 'Fizz' only for multiples of 3
+    def test_print_fizz(self):
+        output = fizzBuzz.fizzBuzz()
+        for i in range(1, 101):
+            if i % 3 == 0 and i % 5 != 0:
+                self.assertEqual(output[i - 1], 'Fizz')
 
 if __name__ == '__main__':
     unittest.main()
