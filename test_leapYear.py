af6c277e22e1420b4519698e940c32459ea2af2e
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:53:45 -0800

    Wrote 2nd leapYear test: 500 is evenly divisible by 4 and 100 and the code should recognize it is not a leap year

--- test_leapYear.py
+++ test_leapYear.py
@@ -5,5 +5,8 @@ class Test_LeapYear(unittest.TestCase):
     def test_not_divisible_by_4(self):
         self.assertEqual(leapYear.leapYear(5), '5 is not a leap year')
 
+    def test_div_by_100_not_400(self):
+        self.assertEqual(leapYear.leapYear(500), '500 is not a leap year')
+
 if __name__ == '__main__':
     unittest.main()
