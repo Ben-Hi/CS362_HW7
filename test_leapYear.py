a4cc7f052b058ec1512174bd3217d81631b1f8fd
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:45:44 -0800

    Wrote 1st leapYear test: input 5 to the code and expect it to say that 5 is not a leap year

--- test_leapYear.py
+++ test_leapYear.py
@@ -1,2 +1,9 @@
 import unittest
 import leapYear
+
+class Test_LeapYear(unittest.TestCase):
+    def test_not_divisible_by_4(self):
+        self.assertEqual(leapYear.leapYear(5), '5 is not a leap year')
+
+if __name__ == '__main__':
+    unittest.main()
