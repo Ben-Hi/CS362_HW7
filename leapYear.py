2330f9da95794d02deed1e6e40bad930c9c04a92
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:55:56 -0800

    Implemented div_by_100_not_400 method: if year mod 4 is 0, the code checks if year is evenly divisible by 100 to check if it is not a leap year

--- leapYear.py
+++ leapYear.py
@@ -2,3 +2,7 @@ def leapYear(year):
     if year % 4 != 0:
         print(year, ' is not a leap year')
         return str(year) + ' is not a leap year'
+    
+    elif year % 100 == 0:
+        print(year, ' is not a leap year')
+        return str(year) + ' is not a leap year'
