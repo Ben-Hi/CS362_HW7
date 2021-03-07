920e63ee0cb2993e0b7a2ece7f24999c36ca554d
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 14:01:11 -0800

    Wrote 4th leapYear test: 4 is only divisible by 4, code should say it is a leap year

--- test_leapYear.py
+++ test_leapYear.py
@@ -11,5 +11,8 @@ class Test_LeapYear(unittest.TestCase):
     def test_div_by_100_and_400(self):
         self.assertEqual(leapYear.leapYear(400), '400 is a leap year')
 
+    def test_only_by_4(self):
+        self.assertEqual(leapYear.leapYear(4), '4 is a leap year')
+
 if __name__ == '__main__':
     unittest.main()
