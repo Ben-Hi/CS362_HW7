3031127320c4accbde82d58a22731a880b355f15
Benjamin Hillen <benjaminrhillen@gmail.com>
2021-03-07 13:59:27 -0800

    Implemented div_by_100_and_400 method: after finding that year mod 4 and year mod 100 = 0, the code will only say the year is a leap year if year mod 400 is 0

--- leapYear.py
+++ leapYear.py
@@ -4,5 +4,10 @@ def leapYear(year):
         return str(year) + ' is not a leap year'
     
     elif year % 100 == 0:
-        print(year, ' is not a leap year')
-        return str(year) + ' is not a leap year'
+        if year % 400 == 0:
+            print(year, ' is a leap year')
+            return str(year) + ' is a leap year'
+        
+        else:
+            print(year, ' is not a leap year')
+            return str(year) + ' is not a leap year'
